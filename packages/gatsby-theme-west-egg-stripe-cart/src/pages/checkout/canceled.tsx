ce8598dc98c74237b28e7e9cfe50128b70b9784c
HAINES-DESKTOP\richa <richardkhaines@hotmail.co.uk>
2019-05-12 09:21:42 +0200

    renamed env file

--- packages/gatsby-theme-west-egg-stripe-cart/src/pages/checkout/canceled.tsx
+++ packages/gatsby-theme-west-egg-stripe-cart/src/pages/checkout/canceled.tsx
@@ -1,11 +1,13 @@
 import * as React from 'react';
 
 export default () => (
-	<div style={{
-		display: 'flex',
-		justifyContent: 'center',
-		alignItems: 'center'
-	}}>
+	<div
+		style={{
+			display: 'flex',
+			justifyContent: 'center',
+			alignItems: 'center',
+		}}
+	>
 		<h1>Purchase Canceled!</h1>
 		<h5>override me as im not pretty...</h5>
 		<h5>to do that you have to shadow me in your site, or theme.</h5>
--- packages/gatsby-theme-west-egg-stripe-cart/src/utils/hooks.ts
+++ packages/gatsby-theme-west-egg-stripe-cart/src/utils/hooks.ts
@@ -7,15 +7,15 @@ import { graphql, useStaticQuery } from 'gatsby';
 export function useSiteMetadata() {
 	const { site } = useStaticQuery(
 		graphql`
-        query SITE_METADATA_QUERY {
-          site {
-            siteMetadata {
-                STRIPE_API_KEY,
-                siteUrl
-            }
-          }
-        }
-      `
+			query SITE_METADATA_QUERY {
+				site {
+					siteMetadata {
+						STRIPE_API_KEY
+						siteUrl
+					}
+				}
+			}
+		`
 	);
 	return site.siteMetadata;
 }
